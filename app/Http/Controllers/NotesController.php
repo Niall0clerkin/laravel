fe9fefcb1e9ef06e9da0434dbe904b7eb0836d59
Niall0clerkin <Niall0clerkin@gmail.com>
2024-04-15 15:57:10 +0100

    as far as edit note blade

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -1,82 +1,103 @@
 <?php
 
 namespace App\Http\Controllers;
-
-
 use App\Models\Note;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class NotesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        $userId = Auth::id();
 
-        $notes = Note::where('user_id', $userId)
+/**
 
-        ->latest('updated_at')
+* Display a listing of the resource.
 
-        ->paginate(3);
+*/
 
-        return view('notes.index')->with('notes',$notes);
-    }
+public function index()
+{
+$userId = Auth::id();
+$notes = Note::where('user_id', $userId)
+->latest('updated_at')
+->paginate(3);
+return view('notes.index')->with('notes',$notes);
+}
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+/**
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+* Show the form for creating a new resource.
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+*/
 
-    {
+public function create()
 
-    $note = Note::where('id',$id)
-    ->where('user_id',Auth::id())
-    ->firstOrFail();
+{
 
-    return view('notes.show')->with('note', $note);
+return view('notes.create');
 
-    }
+}
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
+/**
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
+* Store a newly created resource in storage.
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
+*/
+
+public function store(Request $request)
+{
+return "Want to Store the new Note ";
+}
+
+/**
+
+* Display the specified resource.
+
+*/
+
+public function show(string $id)
+{
+$note = Note::where('id',$id)
+->where('user_id',Auth::id())
+->firstOrFail();
+return view('notes.show')->with('note', $note);
+}
+
+/**
+
+* Show the form for editing the specified resource.
+
+*/
+
+public function edit(string $id)
+
+{
+$note = Note::findOrFail($id);
+if($note->user_id != Auth::id()) {
+return abort(403);
+}
+return view('notes.edit')->with(['note' => $note]);
+}
+
+/**
+
+* Update the specified resource in storage.
+
+*/
+
+public function update(Request $request, string $id)
+{
+return "Want to Update Note " . $id;
+}
+
+/**
+
+* Remove the specified resource from storage.
+
+*/
+
+public function destroy(string $id)
+{
+return "Remove the Note with id = " . $id;
+}
+
 }
--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,14 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::resource('/notes', NotesController::class);
+
+    Route::get('/notes', [NotesController::class, 'index'])->name('notes.index');
+    Route::get('/notes/{id}', [NotesController::class, 'show'])->name('notes.show');
+    Route::get('/notes/create', [NotesController::class, 'create'])->name('notes.create');
+    Route::get('/notes/{id}/edit', [NotesController::class, 'edit'])->name('notes.edit');
+    Route::post('/notes', [NotesController::class, 'store'])->name('notes.store');
+    Route::put('/notes/{id}', [NotesController::class, 'update'])->name('notes.update');
+    Route::delete('/notes/{id}', [NotesController::class, 'destroy'])->name('notes.destroy');
 });
 
 require __DIR__.'/auth.php';
